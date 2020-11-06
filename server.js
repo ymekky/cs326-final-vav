5c0b3abb0d94aa923bd39f8233b4c21e930aff28
YASMEEN <yasmeenmekky@gmail.com>
2020-11-06 23:04:15 +0200

    added endpoint paths

--- server.js
+++ server.js
@@ -8,23 +8,28 @@ if (existsSync("database.json")) {
     database = JSON.parse(readFileSync("database.json"));
 } else {
     database = {
-        wordScores: [],
-        gameScores: []
+        Users: [],
+        Rides: []
     };
 }
 
 createServer(async (req, res) => {
     const parsed = parse(req.url, true);
 
-    if (parsed.pathname === '/user/new') {
+    if (parsed.pathname === '/user/new') { 
         let body = '';
+
         req.on('data', data => body += data);
         req.on('end', () => {
-            const data = JSON.parse(body);
-            database.wordScores.push({
+            console.log(body);
+            const data = JSON.parse(JSON.stringify(body));
+            database.Users.push({
+                user_id: data.USERID,
                 name: data.name,
                 email: data.email,
                 password: data.password,
+                Rides: {},
+                Chatters: []
             });
             
             writeFile("database.json", JSON.stringify(database), err => {
@@ -33,28 +38,37 @@ createServer(async (req, res) => {
                 } else res.end();
             });
         });
-    } else if (parsed.pathname === '/gameScore') {
+    } else if (parsed.pathname === '/user/rides/pending/new') { //?USERID=89&RIDEID=29
         let body = '';
         req.on('data', data => body += data);
         req.on('end', () => {
             const data = JSON.parse(body);
-            database.gameScores.push({
-                name: data.name,
-                score: data.score
-            });
-            
+
+            const my_ride = {};
+            for(let ride of database.Rides) {
+                if (ride.id === data.RIDEID) {
+                    my_ride = ride;
+                }
+            }
+            for(let user of database.Users){
+                if(user.id === data.USERID) {
+                    database.Users.indexOf(user)[Rides]['pending'] = ride;
+                }
+            }
+
             writeFile("database.json", JSON.stringify(database), err => {
                 if (err) {
                     console.err(err);
                 } else res.end();
             });
         });
-    } else {
+    }  
+    else {
         // If the client did not request an API endpoint, we assume we need to fetch a file.
         // This is terrible security-wise, since we don't check the file requested is in the same directory.
         // This will do for our purposes.
         const filename = parsed.pathname === '/' ? "index.html" : parsed.pathname.replace('/', '');
-        const path = join("client/", filename);
+        const path = filename;
         console.log("trying to serve " + path + "...");
         if (existsSync(path)) {
             if (filename.endsWith("html")) {
