8c2a954dd36df9688807fbefb8eacdbb798ea94a
alishbakhalil <55882274+alishbakhalil@users.noreply.github.com>
2020-11-09 00:09:06 -0500

    connected the server to client

--- client/login.js
+++ client/login.js
@@ -6,14 +6,16 @@ window.addEventListener("load", async function() {
 
 
 	    const response = await fetch(`./login?email=${email}&password=${password}`);
-		
+			console.log('response', response)
 	    if (!response.ok) {
 	        console.error("Could not login.");
 	    }
 	    else {
-	    	const user = await response.json();
+				const user = await response.json();
+				console.log('user successfully logged in', user)
 	    	window.localStorage.setItem("logged-in", true);
-	    	window.localStorage.setItem("me",JSON.stringify(user.me));
+				window.localStorage.setItem("me",JSON.stringify(user.me));
+				window.location.replace('./account.html')
 	    }
 	});
 });
