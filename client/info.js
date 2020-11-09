2a5a8826706e5b3164792f6dbbffb299ba3ae889
alishbakhalil <55882274+alishbakhalil@users.noreply.github.com>
2020-11-09 00:19:18 -0500

    /login connected to info.js

---
+++ client/info.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", async function() {
+	document.getElementById("login-click").addEventListener('click', async function () {
+		const email = document.getElementById("email").value;
+		//console.log(email);
+		const password = document.getElementById("password").value;
+
+
+	    const response = await fetch(`./login?email=${email}&password=${password}`);
+			console.log('response', response)
+	    if (!response.ok) {
+	        console.error("Could not login.");
+	    }
+	    else {
+				const user = await response.json();
+				console.log('user successfully logged in', user)
+	    	window.localStorage.setItem("logged-in", true);
+				window.localStorage.setItem("me",JSON.stringify(user.me));
+				window.location.replace('./account.html')
+	    }
+	});
+});
